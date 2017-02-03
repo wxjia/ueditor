1579ceab55f8d732349868eaca140dd1b6f1fdb0
wxjia <jwx1016@qq.com>
2017-02-03 18:44:41 +0800

    my ueditor demo project

---
+++ WebRoot/jsp/ueditor.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <base href="<%=basePath%>">
+    
+    <title>ueditor</title>
+    <meta charset="UTF-8">
+    
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">    
+	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+	<meta http-equiv="description" content="This is my page">
+	<!--
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	-->
+
+  </head>
+  
+  <body>
+  <form action="MyServlet" method="post">
+     <!-- 加载编辑器的容器 -->
+    <script id="container" name="content" type="text/plain">
+        这里写你的初始化内容
+    </script>
+    <input type="submit" value="submit">
+    </form>
+    <!-- 配置文件 -->
+    <script type="text/javascript" src="ueditors/ueditor.config.js"></script>
+    <!-- 编辑器源码文件 -->
+    <script type="text/javascript" src="ueditors/ueditor.all.js"></script>
+    <!-- 实例化编辑器 -->
+    <script type="text/javascript">
+        var ue = UE.getEditor('container');
+    </script>
+  </body>
+</html>
---
+++ src/cn/wxjia/servlet/MyServlet.java
@@ -0,0 +1,53 @@
+package cn.wxjia.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	public MyServlet() {
+		super();
+	}
+
+	public void destroy() {
+		super.destroy();
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("get"+request.getParameter("content"));
+		out.flush();
+		out.close();
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("post"+request.getParameter("content"));
+
+		Map<String, String[]> map = request.getParameterMap();
+		 for (String key : map.keySet()) {
+			 System.out.println(key+" - "+map.get(key)[0]);
+		 }
+
+		out.flush();
+		out.close();
+	}
+
+	public void init() throws ServletException {
+	}
+
+}
